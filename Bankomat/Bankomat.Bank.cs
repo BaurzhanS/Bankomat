eb319e3fd28141b331a955e2320dbb9951b7438c
baurik82 <baurik82@mail.ru>
2019-02-22 17:04:43 +0300

    Add project files.

---
+++ Bankomat/Bankomat.Bank.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Bankomat
+{
+    namespace Bankomat.Bank
+    {
+        public class Bank
+        {
+            Client.Client client = new Client.Client();
+            Account.Account account = new Account.Account();
+
+
+        }
+    }
+}
+    
